92d7a0a1176cf00b9f70129ee5975af307e371af
Ava <154105735+furbygir@users.noreply.github.com>
2025-03-30 20:16:03 +1000

    adding new furby

--- init.lua
+++ init.lua
@@ -742,4 +742,23 @@ minetest.register_craft({
 	}
 })
 
+minetest.register_node('moms_furby:furby', {
+	description = 'Furby - Purple',
+	drawtype = 'mesh',
+	mesh = 'moms_furby.obj',
+	tiles = {'moms_furby.png'},
+	inventory_image = 'moms_furby.png',
+	groups = {oddly_breakable_by_hand=1},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
 
